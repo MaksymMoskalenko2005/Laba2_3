2d0acf10aa91fd91c368948d358fc67401bc1509
1 <1@1->
2023-05-16 22:19:12 +0300

    Добавьте файлы проекта.

---
+++ Laba2_3/Point.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2_3
+{
+    class Point<T> where T : struct, IComparable<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+
+
+        public Point() : this(default(T), default(T))
+        { }
+        public Point(T x, T y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public T getX()
+        {
+            return this.X;
+        }
+        public T getY()
+        {
+            return this.Y;
+        }
+    }
+    internal class Rectangle<T> where T : struct, IComparable<T>
+    {
+        private Point<T> center, A, B, C, D;
+        public double AB, BC, CD, AD, AC;
+
+        public Rectangle(Point<T> a, Point<T> b, Point<T> c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+            this.AB = calculateLength(A, B);
+            this.BC = calculateLength(B, C);
+            this.D = new Point<T>((dynamic)C.X, (dynamic)A.Y);
+            this.CD = calculateLength(C, D);
+            this.AD = calculateLength(A, D);
+            this.AC = calculateLength(A, C);
+            this.center = new Point<T>(((dynamic)A.X + (dynamic)c.X) / 2, ((dynamic)A.Y + (dynamic)C.Y) / 2);
+        }
+
+
+        public double calculatePerimeter()
+        {
+            return AB + BC + CD + AD;
+        }
+
+        public double calculateArea()
+        {
+            return AB * BC;
+        }
+
+        private double calculateLength(Point<T> point1, Point<T> point2)
+        {
+            return Math.Sqrt(PowDouble(point1.X, point2.X) + PowDouble(point1.Y, point2.Y));
+        }
+
+        private double PowDouble(T point1, T point2)
+        {
+            return Math.Pow(Convert.ToDouble(point1) - Convert.ToDouble(point2), 2);
+        }
+
+        public void changePoints(Point<T> a, Point<T> b, Point<T> c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+            this.AB = calculateLength(A, B);
+            this.BC = calculateLength(B, C);
+            this.D = new Point<T>((dynamic)C.X, (dynamic)A.Y);
+            this.CD = calculateLength(C, D);
+            this.AD = calculateLength(A, D);
+            this.center = new Point<T>(((dynamic)A.X + (dynamic)c.X) / 2, ((dynamic)A.Y + (dynamic)C.Y) / 2);
+        }
+        public double ABplusBCPow2() { return Math.Pow(AB,2)  + Math.Pow(BC, 2); }
+        public double ABplusACPow2() { return Math.Pow(AB, 2) + Math.Pow(AC, 2); }
+        public double ACplusBCPow2() { return Math.Pow(AC, 2) + Math.Pow(BC, 2); }
+        public bool check()
+        {
+            if ((ABplusBCPow2()==Math.Round(Math.Pow(AC,2)))||(ABplusACPow2() == Math.Round(Math.Pow(BC, 2)))||(ACplusBCPow2() == Math.Round(Math.Pow(AB, 2))))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+    }
+}
---
+++ Laba2_3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.Drawing;
+
+namespace Laba2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<double> a = new Point<double>(0, 0);
+            Point<double> b = new Point<double>(0, 0);
+            Point<double> c = new Point<double>(0, 0);
+            Rectangle<double> r = new Rectangle<double>(a, b, c);
+            while(true)
+            {
+                Console.WriteLine("Значення типу double");
+                Console.Write("Ax:"); string AxStr = Console.ReadLine(); double Ax = double.Parse(AxStr);
+                Console.Write("Ay:"); string AyStr = Console.ReadLine(); double Ay = double.Parse(AyStr);
+                Console.Write("Bx:"); string BxStr = Console.ReadLine(); double Bx = double.Parse(BxStr);
+                Console.Write("By:"); string ByStr = Console.ReadLine(); double By = double.Parse(ByStr);
+                Console.Write("Cx:"); string CxStr = Console.ReadLine(); double Cx = double.Parse(CxStr);
+                Console.Write("Cy:"); string CyStr = Console.ReadLine(); double Cy = double.Parse(CyStr);
+                a = new Point<double>(Ax, Ay);
+                b = new Point<double>(Bx, By);
+                c = new Point<double>(Cx, Cy);
+                r = new Rectangle<double>(a,b,c);
+                if (r.check())
+                {
+                    Console.WriteLine("Трикутник прямокутний");
+                    break;
+                }
+                else { Console.WriteLine("Не є прямокутним трикутником"); }
+            }
+            Console.WriteLine("area: площа прямокутника");
+            Console.WriteLine("perimeter: периметр прямокутника");
+            Console.WriteLine("stop: закінчення");
+
+            while (true)
+            {
+                string input1 = Console.ReadLine();
+                if (input1 == "area") { Console.WriteLine(r.calculateArea()); }
+                if (input1 == "perimeter") { Console.WriteLine(r.calculatePerimeter()); }
+                if (input1 == "stop") { break; }
+
+            }
+            Point<int> a1 = new Point<int>(0, 0);
+            Point<int> b1 = new Point<int>(0, 0);
+            Point<int> c1 = new Point<int>(0, 0);
+            Rectangle<int> r1 = new Rectangle<int>(a1, b1, c1);
+            while (true)
+            {
+                Console.WriteLine("Значення типу int");
+                Console.Write("Ax:"); string AxStr1 = Console.ReadLine(); int Axint = int.Parse(AxStr1);
+                Console.Write("Ay:"); string AyStr1 = Console.ReadLine(); int Ayint = int.Parse(AyStr1);
+                Console.Write("Bx:"); string BxStr1 = Console.ReadLine(); int Bxint = int.Parse(BxStr1);
+                Console.Write("By:"); string ByStr1 = Console.ReadLine(); int Byint = int.Parse(ByStr1);
+                Console.Write("Cx:"); string CxStr1 = Console.ReadLine(); int Cxint = int.Parse(CxStr1);
+                Console.Write("Cy:"); string CyStr1 = Console.ReadLine(); int Cyint = int.Parse(CyStr1);
+                a1 = new Point<int>(Axint, Ayint);
+                b1 = new Point<int>(Bxint, Byint);
+                c1 = new Point<int>(Cxint, Cyint);
+                r1 = new Rectangle<int>(a1, b1, c1);
+                Console.WriteLine(Math.Pow(r1.AB, 2) + Math.Pow(r1.BC, 2));
+                Console.WriteLine(Math.Pow(r1.AC, 2));
+                if (r.check())
+                {
+                    Console.WriteLine("Трикутник прямокутний");
+                    break;
+                }
+                else { Console.WriteLine("Не є прямокутним трикутником"); }
+            }
+            Console.WriteLine("area: площа прямокутника");
+            Console.WriteLine("perimeter: периметр прямокутника");
+            Console.WriteLine("stop: закінчення");
+
+            while (true)
+            {
+                string input2 = Console.ReadLine();
+                if (input2 == "area") { Console.WriteLine(r1.calculateArea()); }
+                if (input2 == "perimeter") { Console.WriteLine(r1.calculatePerimeter()); }
+                if (input2 == "stop") { break; }
+
+            }
+
+
+        }
+    }
+}
